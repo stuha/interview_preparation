3f5f1ed2147eabf9ae6850847a45a4779730b6cb
Jason Wang <1724555125@qq.com>
2018-02-12 17:50:40 +0800

    Hard work, isn't it! Take a rest!

--- C/singly_linked_list.c
+++ C/singly_linked_list.c
@@ -26,6 +26,8 @@ struct list *init()
 	struct list *ltmp;
 
 	ltmp = malloc(sizeof(struct list));
+	if (!ltmp)
+		exit(1);
 	memset(ltmp, 0, sizeof(struct list));
 
 	ltmp->head = NULL;
@@ -72,6 +74,8 @@ int push_front(struct list *l, int value)
 	struct node *n;
 
 	n = malloc(sizeof(struct node));
+	if (!n)
+		return 1;
 	memset(n, 0, sizeof(struct node));
 
 	n->data = value;
@@ -103,6 +107,8 @@ int push_back(struct list *l, int value)
 	struct node *n;
 
 	n = malloc(sizeof(struct node));
+	if (!n)
+		return 1;
 	memset(n, 0, sizeof(struct node));
 
 	n->data = value;
@@ -134,6 +140,172 @@ int pop_back(struct list *l)
 	return dtmp;
 }
 
+int front(struct list *l)
+{
+	value_at(l, 0);
+}
+
+int back(struct list *l)
+{
+	value_at(l, l->size - 1);
+}
+
+bool insert(struct list *l, int index, int value)
+{
+	if (index < 0) {
+		printf("Are you kidding me?!\n");
+		return false;
+	}
+
+	if (index > l->size - 1) {
+		printf("Your index is out of range!\n");
+		return false;
+	}
+
+	if (index == 0)
+		push_front(l, value);
+	else {
+		struct node *nnew = malloc(sizeof(struct node));
+		if (!nnew)
+			return false;
+		memset(nnew, 0, sizeof(struct node));
+		nnew->data = value;
+		/* ntmp is at index 0 */
+		struct node *ntmp = l->head;
+		for (int i = 0; i < index - 1; i++)
+			ntmp = ntmp->next;
+
+		nnew->next = ntmp->next;
+		ntmp->next = nnew;
+
+		l->size++;
+	}
+
+	return true;
+}
+
+bool erase(struct list *l, int index)
+{
+	if (index < 0) {
+		printf("You are kidding me!\n");
+		return false;
+	}
+	if (index > l->size - 1) {
+		printf("Your index is out of range!\n");
+		return false;
+	}
+	if (index == 0) {
+		free(l->head);
+		l->head = l->head->next;
+
+		l->size--;
+	} else {
+		struct node *ntmp = l->head;
+		/* find the previous of the index item */
+		for (int i = 0; i < index - 1; i++)
+			ntmp = ntmp->next;
+		free(ntmp->next);
+		ntmp->next = ntmp->next->next;
+
+		l->size--;
+	}
+	return true;
+}
+
+int value_n_from_end(struct list *l, int npos)
+{
+	if (npos < 0) {
+		printf("Kidding!\n");
+		return -1;
+	}
+	if (npos > l->size - 1) {
+		printf("Out of range!\n");
+		return -1;
+	}
+
+	struct node *ntmp = l->head;
+	for (int i = 1; i < l->size - npos; i++)
+		ntmp = ntmp->next;
+
+	return ntmp->data;
+}
+
+void reverse(struct list *l)
+{
+	struct node *prev, *cur;
+	if (l->head != NULL) {
+		prev = l->head;
+		cur = l->head->next;
+		l->head = l->head->next;
+
+		prev->next = NULL;
+
+		while (l->head != NULL) {
+			l->head = l->head->next;
+			cur->next = prev;
+
+			prev = cur;
+			cur = l->head;
+		}
+
+		l->head = prev;
+	}
+}
+
+int remvoe_value(struct list *l, int value)
+{
+	if (l->head != NULL) {
+		struct node *cur = l->head;
+		struct node *prev = l->head;
+		if (cur->data == value) {
+			free(cur);
+			l->head = cur->next;
+			l->size--;
+			return 1;
+		}
+		while (cur->next != NULL) {
+			prev = cur;
+			cur = cur->next;
+			if (cur->data == value) {
+				free(cur);
+				prev->next = cur->next;
+				l->size--;
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+bool destroy(struct list *l)
+{
+	for (int i = 1; i <= l->size; i++)
+		if (erase(l, l->size - i) == false)
+			return false;
+
+	free(l);
+
+	return true;
+}
+
+
+/* Testing */
+void show_all(struct list *l)
+{
+	struct node *ntmp = l->head;
+	int i = 1;
+
+	printf("\nHEAD -> ");
+	printf("0: %d -> ", ntmp->data);
+	while (ntmp->next != NULL) {
+		ntmp = ntmp->next;
+		printf("%d: %d -> ", i, ntmp->data);
+		i++;
+	}
+	printf("|||\n");
+}
+
 int main(void)
 {
 	struct list *testl;
@@ -155,8 +327,55 @@ int main(void)
 
 	int c = value_at(testl, 1);
 	printf("c: %d\n", c);
-	int d = value_at(testl, 2);
-	printf("d: %d\n", d);
+	//int d = value_at(testl, 2); // yes, out of range!
+	//printf("d: %d\n", d);
 
-	//int c = value_at(testl, 1);
+	printf("front: %d\n", front(testl));
+	printf("back: %d\n", back(testl));
+
+	insert(testl, 0, 100);
+	insert(testl, 1, 200);
+	insert(testl, -10, 300);
+
+	show_all(testl);
+	erase(testl, 0);
+	show_all(testl);
+	erase(testl, 2);
+	show_all(testl);
+
+	insert(testl, 0, 100);
+	insert(testl, 1, 200);
+	insert(testl, 3, 300);
+	erase(testl, 1);
+	insert(testl, 3, 10);
+	insert(testl, 4, 20);
+	insert(testl, 5, 30);
+	show_all(testl);
+
+	printf("n0: %d\n", value_n_from_end(testl, 0));
+	printf("n2: %d\n", value_n_from_end(testl, 2));
+	printf("n3: %d\n", value_n_from_end(testl, 3));
+
+	reverse(testl);
+	show_all(testl);
+	push_back(testl, 5);
+
+	remvoe_value(testl, 5);
+	show_all(testl);
+	remvoe_value(testl, 300);
+	show_all(testl);
+	remvoe_value(testl, 100);
+	show_all(testl);
+
+	destroy(testl);
+#if 0
+	struct list *testl2 = init();
+	push_front(testl2, 100);
+	reverse(testl2);
+	show_all(testl2);
+
+	destroy(testl2);
+	/* Now, segmentation fault */
+	//show_all(testl);
+#endif
 }
--- theory/how_to_implement_list.md
+++ theory/how_to_implement_list.md
@@ -33,3 +33,18 @@ struct node {
 
 ### Questions and tips
 
+- [x] implement (I did with tail pointer & without):
+    - [x] size() - returns number of data elements in list
+    - [x] empty() - bool returns true if empty
+    - [x] value_at(index) - returns the value of the nth item (starting at 0 for first)
+    - [x] push_front(value) - adds an item to the front of the list
+    - [x] pop_front() - remove front item and return its value
+    - [x] push_back(value) - adds an item at the end
+    - [x] pop_back() - removes end item and returns its value
+    - [x] front() - get value of front item
+    - [x] back() - get value of end item
+    - [x] insert(index, value) - insert value at index, so current item at that index is pointed to by new item at index
+    - [x] erase(index) - removes node at given index
+    - [x] value_n_from_end(n) - returns the value of the node at nth position from the end of the list
+    - [x] reverse() - reverses the list
+    - [x] remove_value(value) - removes the first item in the list with this value
