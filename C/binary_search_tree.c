417eb4064969500fc4514aba2fa7ef7355bbba34
Jason Wang <1724555125@qq.com>
2018-03-10 21:56:04 +0800

    never give up

--- C/binary_search_tree.c
+++ C/binary_search_tree.c
@@ -79,22 +79,120 @@ void print_values(struct bst_node *root)
 	printf("%d\n", root->value);
 	print_values(root->right);
 }
-/*
-get_height
 
-get_min
+int obtain_max(int v1, int v2)
+{
+	return v1 >= v2 ? v1 : v2;
+}
 
-get_max
+int get_height(struct bst_node *root)
+{
+	if (root == NULL)
+		return 0;
 
-is_binary_search_tree
+	return 1 + obtain_max(get_height(root->left), get_height(root->right));
+}
 
-delete_value
+/* return the most left value */
+int get_min(struct bst_node *root)
+{
+	if (root == NULL)
+		return -1;
 
-get_successor
+	struct bst_node *current = root;
+	while (current->left != NULL)
+		current = current->left;
 
-*/
+	return current->value;
+}
 
+int get_max(struct bst_node *root)
+{
+	if (root == NULL)
+		return -1;
 
+	struct bst_node *current = root;
+	while (current->right != NULL)
+		current = current->right;
+
+	return current->value;
+}
+
+bool is_binary_search_tree(struct bst_node *root, int min_value, int max_value)
+{
+	if (root == NULL) return true;
+	if (root->value < min_value || root->value > max_value) return false;
+
+	return is_binary_search_tree(root->left, min_value, root->value - 1) &&
+		is_binary_search_tree(root->right, root->value + 1, max_value);
+}
+
+/* more complicated than other function */
+struct bst_node *delete_value(struct bst_node *root, int value)
+{
+	if (root == NULL) return root;
+
+	if (root->value > value)
+		root->left = delete_value(root->left, value);
+	else if (root-> value < value)
+		root->right = delete_value(root->right, value);
+	else {
+		if (root->left == NULL && root->right == NULL) {
+			free(root);
+			root = NULL;
+		} else if (root->left == NULL) {
+			struct bst_node *tmp = root;
+			root = root->right;
+			free(tmp);
+		} else if (root->right == NULL) {
+			struct bst_node *tmp = root;
+			root = root->left;
+			free(tmp);
+		} else {
+			/* get the minimal value in the right subtree */
+			int right_min = get_min(root->right);
+			root->value = right_min;
+			root->right = delete_value(root->right, right_min);
+		}
+	}
+
+	return root;
+}
+
+int get_successor(struct bst_node *node, int value)
+{
+	if (node == NULL) return -1;
+
+	struct bst_node *target = node;
+
+	while (target->value != value) {
+		if (value < target->value) {
+			target = target->left;
+		} else if (value > target->value) {
+			target = target->right;
+		}
+	}
+
+	// arrived at target node
+	if (target->right != NULL) {
+		// get min value of right subtree
+		return get_min(target->right);
+	} else {
+		// get lowest ancestor that is a left child in the path to target value
+		struct bst_node *successor = NULL;
+		struct bst_node *ancestor = node;
+		while (ancestor != NULL) {
+			if (value < ancestor->value) {
+				successor = ancestor;
+				ancestor = ancestor->left;
+			} else {
+				ancestor = ancestor->right;
+			}
+		}
+
+		return successor->value;
+	}
+}
 
 
 /* For testing */
